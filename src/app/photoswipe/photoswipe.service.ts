ba5503f25d42c6a811085f8752bbe889595ae54b
Paul Friederichsen <floogulinc@gmail.com>
2020-03-28 06:25:41 -0500

    Add scroll wheel support for the image viewer
    Scroll up/down goes to previous/next image
    Middle/scroll wheel click closes photoswipe

--- src/app/photoswipe/photoswipe.service.ts
+++ src/app/photoswipe/photoswipe.service.ts
@@ -3,23 +3,32 @@ import { PhotoswipeComponent } from './photoswipe.component';
 import * as PhotoSwipe from 'photoswipe';
 import * as PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
 import { HydrusFile } from '../hydrus-file';
+import { fromEvent, Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoswipeService {
 
+  private photoswipeComponent: ComponentRef<PhotoswipeComponent>;
+  private pspElement: ElementRef;
+
+  ps: PhotoSwipe<{}>;
+
+  public onMouseWheel$: Observable<WheelEvent>;
+  public onMouse$: Observable<MouseEvent>;
+  public psClose$ = new Subject();
+
   constructor(private applicationRef: ApplicationRef, private injector: Injector, private resolver: ComponentFactoryResolver) {
     this.photoswipeComponent = this.resolver.resolveComponentFactory(PhotoswipeComponent).create(this.injector);
     this.pspElement = this.photoswipeComponent.instance.pspElement;
     this.applicationRef.attachView(this.photoswipeComponent.hostView);
     document.body.appendChild(this.photoswipeComponent.location.nativeElement);
+
+    this.onMouseWheel$ = fromEvent<WheelEvent>(this.photoswipeComponent.location.nativeElement, 'wheel');
+    this.onMouse$ = fromEvent<MouseEvent>(this.photoswipeComponent.location.nativeElement, 'auxclick');
   }
-
-  private photoswipeComponent: ComponentRef<PhotoswipeComponent>;
-  private pspElement: ElementRef;
-
-  ps: PhotoSwipe<{}>;
 
   getPhotoSwipeItems(items : HydrusFile[]) : PhotoSwipe.Item[] {
     return items.map((i) => this.getPhotoSwipeItem(i));
@@ -50,7 +59,23 @@ export class PhotoswipeService {
       hideAnimationDuration:0,
       showAnimationDuration:0
     });
+    ps.listen('close', () => {
+      this.psClose$.next();
+    });
     ps.init();
+    this.onMouseWheel$.pipe(takeUntil(this.psClose$)).subscribe((event) => {
+      if(event.deltaY < 0) { //wheel up
+        ps.prev();
+      } else if (event.deltaY > 0) { //wheel down
+        ps.next();
+      }
+    });
+    this.onMouse$.pipe(takeUntil(this.psClose$)).subscribe((event) => {
+      console.log(event);
+      if(event.button == 1) {
+        ps.close();
+      }
+    })
   }
 
 }
