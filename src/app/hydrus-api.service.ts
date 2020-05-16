76fdf66e3d8e84344ae00034598f78ac0b3618e7
Paul Friederichsen <floogulinc@gmail.com>
2020-05-16 01:46:13 -0500

    Clean up and add comments to the Hydrus API service
    Fixes a bunch of TSLint warnings

--- src/app/hydrus-api.service.ts
+++ src/app/hydrus-api.service.ts
@@ -27,24 +27,24 @@ export class HydrusApiService {
 
 
   public getAPIUrl(): string {
-    return this.hydrusApiUrl + (this.hydrusApiUrl.endsWith("/") ? "" : "/");
+    return this.hydrusApiUrl + (this.hydrusApiUrl.endsWith('/') ? '' : '/');
   }
 
-  private getHeaders() : HttpHeaders {
+  private getHeaders(): HttpHeaders {
     return new HttpHeaders({
-      "Hydrus-Client-API-Access-Key" : this.hydrusApiKey
+      'Hydrus-Client-API-Access-Key' : this.hydrusApiKey
     });
   }
 
   private apiGet(path: string, params?: HttpParams) {
     return this.http.get(this.getAPIUrl() + path, {
-      params: params,
+      params,
       headers: this.getHeaders()
     });
   }
 
   public testApi(): Observable<HydrusKeyVerificationData> {
-    return this.http.get<HydrusKeyVerificationData>(this.getAPIUrl() + "verify_access_key", {
+    return this.http.get<HydrusKeyVerificationData>(this.getAPIUrl() + 'verify_access_key', {
       headers: this.getHeaders()
     });
   }
@@ -55,51 +55,73 @@ export class HydrusApiService {
    *
    * Search for the client's files.
    * @param tags (a list of tags you wish to search for)
-   * @param [system_inbox] true or false (optional, defaulting to false)
-   * @param [system_archive] true or false (optional, defaulting to false)
+   * @param system_inbox true or false (optional, defaulting to false)
+   * @param system_archive true or false (optional, defaulting to false)
    * @returns The full list of numerical file ids that match the search.
    */
   public searchFiles(tags: string, system_inbox?: string, system_archive?: string) {
-    let httpParams: HttpParams = new HttpParams().set("tags", tags);
-    if(system_inbox) httpParams = httpParams.set("system_inbox", system_inbox);
-    if(system_archive) httpParams = httpParams.set("system_archive", system_archive);
-    return this.apiGet("get_files/search_files", httpParams);
+    let httpParams: HttpParams = new HttpParams().set('tags', tags);
+    if (system_inbox) { httpParams = httpParams.set('system_inbox', system_inbox); }
+    if (system_archive) { httpParams = httpParams.set('system_archive', system_archive); }
+    return this.apiGet('get_files/search_files', httpParams);
   }
 
   /**
    * GET /get_files/file_metadata
    *
    * Get metadata about files in the client.
-   * @param [file_ids] (a list of numerical file ids)
-   * @param [hashes] (a list of hexadecimal SHA256 hashes)
-   * @param [only_return_identifiers] true or false (optional, defaulting to false)
+   * @param file_ids (a list of numerical file ids)
+   * @param hashes (a list of hexadecimal SHA256 hashes)
+   * @param only_return_identifiers true or false (optional, defaulting to false)
    * @returns  A list of JSON Objects that store a variety of file metadata.
    */
   public getFileMetadata(file_ids?: string, hashes?: string, only_return_identifiers?: string) {
     let httpParams: HttpParams = new HttpParams();
-    if(file_ids) httpParams = httpParams.set("file_ids", file_ids);
-    if(hashes) httpParams = httpParams.set("hashes", hashes);
-    if(only_return_identifiers) httpParams = httpParams.set("only_return_identifiers", only_return_identifiers);
+    if (file_ids) { httpParams = httpParams.set('file_ids', file_ids); }
+    if (hashes) { httpParams = httpParams.set('hashes', hashes); }
+    if (only_return_identifiers) { httpParams = httpParams.set('only_return_identifiers', only_return_identifiers); }
 
-    return this.apiGet("get_files/file_metadata", httpParams);
+    return this.apiGet('get_files/file_metadata', httpParams);
   }
 
+  /**
+   * Generates a file's URL from its ID
+   * @param file_id the Hydrus ID of the file to get
+   * @return the URL of the raw full file referenced by the ID
+   */
   public getFileURL(file_id: number): string {
     return this.getAPIUrl() + 'get_files/file?file_id=' + file_id + '&Hydrus-Client-API-Access-Key=' + this.hydrusApiKey;
   }
 
+  /**
+   * Generates a file's thumbnail URL from its ID
+   * @param file_id the Hydrus ID of the file to get a thumbnail of
+   * @return the URL of the thumbnail for the file referenced by the ID
+   */
   public getThumbnailURL(file_id: number): string {
-    return this.getAPIUrl() + 'get_files/thumbnail?file_id=' + file_id + '&Hydrus-Client-API-Access-Key=' + this.hydrusApiKey
+    return this.getAPIUrl() + 'get_files/thumbnail?file_id=' + file_id + '&Hydrus-Client-API-Access-Key=' + this.hydrusApiKey;
   }
 
+  /**
+   * GET /manage_pages/get_pages
+   *
+   * Get the page structure of the current UI session.
+   */
   public getPages() {
-    return this.apiGet("manage_pages/get_pages");
+    return this.apiGet('manage_pages/get_pages');
   }
 
+  /**
+   * GET /manage_pages/get_page_info
+   *
+   * Get information about a specific page.
+   * @param page_key (hexadecimal page_key as stated in /manage_pages/get_pages)
+   * @param simple true or false (optional, defaulting to true)
+   */
   public getPageInfo(page_key: string, simple: string) {
-    let httpParams: HttpParams = new HttpParams().set("page_key", page_key);
-    if(simple) httpParams = httpParams.set("simple", simple);
-    return this.apiGet("manage_pages/get_page_info", httpParams);
+    let httpParams: HttpParams = new HttpParams().set('page_key', page_key);
+    if (simple) { httpParams = httpParams.set('simple', simple); }
+    return this.apiGet('manage_pages/get_page_info', httpParams);
   }
 
 
