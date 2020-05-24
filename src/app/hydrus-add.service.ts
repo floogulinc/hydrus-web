7c1d12909ab9c2e1dc8c785920f9aee73414ac6a
Paul Friederichsen <floogulinc@gmail.com>
2020-05-24 05:09:39 -0500

    Add functions to Hydrus Add service

--- src/app/hydrus-add.service.ts
+++ src/app/hydrus-add.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HydrusApiService } from './hydrus-api.service';
+import { Observable } from 'rxjs';
+import { HydrusURLInfo, HydrusURLFiles, HydrusURLServiceNamesToTags, HydrusAddURLResponse } from './hydrus-url';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,24 @@ import { HydrusApiService } from './hydrus-api.service';
 export class HydrusAddService {
 
   constructor(private api: HydrusApiService) { }
+
+  public getUrlInfo(url: string): Observable<HydrusURLInfo> {
+    return this.api.getUrlInfo(url) as Observable<HydrusURLInfo>;
+  }
+
+  public getUrlFiles(url: string): Observable<HydrusURLFiles> {
+    return this.api.getUrlFiles(url) as Observable<HydrusURLFiles>;
+  }
+
+  public addUrl(url: string, options?: {
+      destination_page_key?: string,
+      destination_page_name?: string,
+      show_destination_page?: string
+      service_names_to_tags?: HydrusURLServiceNamesToTags
+    }): Observable<HydrusAddURLResponse> {
+
+    return this.api.addUrl({url, ...options}) as Observable<HydrusAddURLResponse>;
+  }
 
 
 }
--- src/app/hydrus-url.ts
+++ src/app/hydrus-url.ts
@@ -1,10 +1,12 @@
+export interface HydrusURLFile {
+  status: 0 | 2 | 3;
+  hash: string;
+  note: string;
+}
+
 export interface HydrusURLFiles {
   normalised_url: string;
-  url_file_statuses: {
-    status: 0 | 2 | 3;
-    hash: string;
-    note: string;
-  }[];
+  url_file_statuses: HydrusURLFile[];
 }
 
 export interface HydrusURLInfo {
@@ -15,3 +17,11 @@ export interface HydrusURLInfo {
   can_parse: boolean;
 }
 
+export interface HydrusURLServiceNamesToTags {
+  [service: string]: string[];
+}
+
+export interface HydrusAddURLResponse {
+  human_result_text: string;
+  normalised_url: string;
+}
