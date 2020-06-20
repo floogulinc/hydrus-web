027497f4c645f34739b82fd8f1971703bb64f64e
Paul Friederichsen <floogulinc@gmail.com>
2020-06-20 03:02:52 -0500

    Add known URLs to file info sheet

--- src/app/file-info-sheet/file-info-sheet.component.ts
+++ src/app/file-info-sheet/file-info-sheet.component.ts
@@ -3,6 +3,19 @@ import { HydrusFile } from '../hydrus-file';
 import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 import { TagUtils } from '../tag-utils';
 
+
+type ShareData = {
+  title?: string;
+  text?: string;
+  url?: string;
+};
+
+declare global {
+  interface Navigator {
+      share(data?: ShareData): Promise<void>;
+  }
+}
+
 @Component({
   selector: 'app-file-info-sheet',
   templateUrl: './file-info-sheet.component.html',
@@ -13,5 +26,15 @@ export class FileInfoSheetComponent {
   tagUtils = TagUtils;
 
   constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: {file: HydrusFile}) { }
+
+  navigatorShare = navigator.share;
+
+  shareUrl(url: string) {
+    if (navigator.share) {
+      navigator.share({
+        url
+      });
+    }
+  }
 
 }
