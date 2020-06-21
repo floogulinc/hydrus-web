bba45ab64e5edf1494453e66f1984f995887877f
Paul Friederichsen <floogulinc@gmail.com>
2020-06-21 05:28:40 -0500

    Encode tags correctly when searching

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -13,7 +13,7 @@ export class SearchService {
 
   public searchFiles(tags: string[], options?: {system_inbox?: boolean, system_archive?: boolean}): Observable<number[]> {
     return this.api.searchFiles(
-      JSON.stringify(tags),
+      encodeURIComponent(JSON.stringify(tags)),
       {
         system_inbox: options && options.system_inbox ? "true" : "false",
         system_archive: options && options.system_archive ? "true" : "false"
