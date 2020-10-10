3b3a3cc9cb374163978d175bc47fb2aba7a8e2c8
Paul Friederichsen <floogulinc@gmail.com>
2020-10-10 18:39:36 -0500

    Revert "Temporarily remove saucenao function from Send to Hydrus"
    This reverts commit c19aeb8fa9efe4273726fc78ce737c7387030fbf.

--- src/app/send/send.component.ts
+++ src/app/send/send.component.ts
@@ -6,6 +6,7 @@ import { of, forkJoin } from 'rxjs';
 import { HydrusURLInfo, HydrusURLFiles } from '../hydrus-url';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { SaucenaoService, SaucenaoResults } from '../saucenao.service';
 
 @Component({
   selector: 'app-send',
@@ -17,11 +18,14 @@ export class SendComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   public static urlRegex: RegExp = /([-a-zA-Z0-9^\p{L}\p{C}\u00a1-\uffff@:%_\+.~#?&//=]{2,256}){1}(\.[a-z]{2,4}){1}(\:[0-9]*)?(\/[-a-zA-Z0-9\u00a1-\uffff\(\)@:%,_\+.~#?&//=]*)?([-a-zA-Z0-9\(\)@:%,_\+.~#?&//=]*)?/;
 
+  public saucenaoLoading = false;
+
   constructor(
     private addService: HydrusAddService,
     private route: ActivatedRoute,
     private snackbar: MatSnackBar,
     private router: Router,
+    private saucenaoService: SaucenaoService
   ) { }
 
   currentUrlInfo: HydrusURLInfo;
@@ -38,6 +42,8 @@ export class SendComponent implements OnInit {
   get sendUrl() {
     return this.sendForm.get('sendUrl');
   }
+
+  saucenaoResults: SaucenaoResults[];
 
   ngOnInit(): void {
     this.sendUrl.valueChanges.pipe(
@@ -71,6 +77,7 @@ export class SendComponent implements OnInit {
 
   resetForm() {
     this.sendUrl.reset();
+    this.saucenaoResults = null;
     this.router.navigate(['/send'], { replaceUrl: true });
   }
 
@@ -98,6 +105,22 @@ export class SendComponent implements OnInit {
     this.send(this.sendForm.value.sendUrl, true);
   }
 
-
+  saucenaoLookup() {
+    this.saucenaoLoading = true;
+    this.saucenaoResults = null;
+    const lookupUrl = this.sendForm.value.sendUrl;
+    this.saucenaoService.search(lookupUrl).subscribe(
+      results => {
+        this.saucenaoLoading = false;
+        this.saucenaoResults = results;
+      },
+      err => {
+        this.saucenaoLoading = false;
+        this.snackbar.open('Error: ' + err.message, undefined, {
+          duration: 5000
+        });
+        console.log(err);
+      });
+  }
 
 }
