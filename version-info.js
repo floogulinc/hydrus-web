bb0c8060009c893f68adda0d598efb37e07e1bfa
Paul Friederichsen <floogulinc@gmail.com>
2020-11-20 06:57:17 -0600

    Remove old commented code in version script

--- version-info.js
+++ version-info.js
@@ -4,35 +4,6 @@ const { resolve, relative } = require('path');
 const { writeFileSync } = require('fs-extra');
 const branch = require('git-branch');
 
-// // Mostly from this article: https://medium.com/@amcdnl/version-stamping-your-app-with-the-angular-cli-d563284bb94d
-
-// var info;
-// if(process.env.VERCEL_GITHUB_DEPLOYMENT) {
-//   console.log(`Detected GitHub deployment on Vercel. URL: ${process.env.VERCEL_URL}`);
-//   gitInfo = {
-//       infotype: "vercel-github",
-//       gitinfo: {
-//           VERCEL_URL: process.env.VERCEL_URL,
-//           VERCEL_GITHUB_DEPLOYMENT: process.env.VERCEL_GITHUB_DEPLOYMENT,
-//           VERCEL_GITHUB_COMMIT_REF: process.env.VERCEL_GITHUB_COMMIT_REF,
-//           VERCEL_GITHUB_COMMIT_SHA: process.env.VERCEL_GITHUB_COMMIT_SHA,
-//       },
-//       hash: process.env.VERCEL_GITHUB_COMMIT_SHA,
-//       branch: process.env.VERCEL_GITHUB_COMMIT_REF
-//   }
-// } else {
-//   gitInfo = {
-//       infotype: "git-describe",
-//       gitinfo: gitDescribeSync({
-//           customArguments: ['--abbrev=40'],
-//           dirtyMark: false,
-//           dirtySemver: false
-//       }),
-//       branch: branch.sync()
-//   };
-//   gitInfo.hash = gitInfo.gitinfo.hash.substring(1);
-// }
-
 var gitInfo = gitDescribeSync({
   customArguments: ['--abbrev=40'],
   dirtyMark: false,
