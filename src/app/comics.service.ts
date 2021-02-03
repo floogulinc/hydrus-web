8134b03e1a0934fe3713cabaa77300145e33dc9c
Paul Friederichsen <floogulinc@gmail.com>
2021-02-03 06:37:29 -0600

    Limit concurrency when searching for comics

--- src/app/comics.service.ts
+++ src/app/comics.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HydrusFilesService } from './hydrus-files.service';
 import { SearchService } from './search.service';
 import { forkJoin, from } from 'rxjs';
-import { map, switchMap, filter, mergeMap, toArray, delay, concatMap, tap, reduce, distinct } from 'rxjs/operators';
+import { map, switchMap, filter, mergeMap, toArray, delay, concatMap, tap, reduce, distinct, retry } from 'rxjs/operators';
 import { TagUtils } from './tag-utils';
 import { HydrusFile } from './hydrus-file';
 
@@ -13,6 +13,7 @@ export interface FlatComic {
   numFiles: number;
 }
 
+const CONCURRENCY = 8;
 
 @Injectable({
   providedIn: 'root'
@@ -94,7 +95,7 @@ export class ComicsService {
       mergeMap(tag => this.searchService.searchFiles([tag]).pipe(
         tap(() => this.updateLoading(tag)),
         map(files => ({tag, files}))
-      )))
+      ), CONCURRENCY))
     .pipe(
       filter(({files}) => files.length > 3),
       distinct(({files}) => files.toString()),
@@ -115,7 +116,7 @@ export class ComicsService {
           coverFile: this.findCoverFile(fileMetadata),
           numFiles: fileMetadata.length
         }))
-      )),
+      ), CONCURRENCY),
       reduce((acc, val) => acc.concat([{tag: val.tag, coverFile: val.coverFile, numFiles: val.numFiles}, ...val.volumes.map(v => ({
         tag: val.tag,
         volume: v.tag,
