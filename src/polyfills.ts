91a52b7b9c35833353c7c6d8f9c221cd759320f0
Paul Friederichsen <floogulinc@gmail.com>
2022-02-05 14:06:47 -0600

    @angular/cli migration - update-zonejs
    Update 'zone.js' to version 0.11.x. Read more about this here: https://github.com/angular/angular/blob/master/packages/zone.js/CHANGELOG.md#breaking-changes-since-zonejs-v0111

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,7 +58,7 @@ import 'array-flat-polyfill';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
