5cab05dfc758b735d219ca69609b0a6ee1f5dd0c
Paul Friederichsen <floogulinc@gmail.com>
2022-12-14 22:25:46 -0600

    Don't extend existing note when adding

--- src/app/hydrus-notes.service.ts
+++ src/app/hydrus-notes.service.ts
@@ -2,22 +2,25 @@ import { Injectable } from '@angular/core';
 import { HydrusApiService } from './hydrus-api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HydrusNotesService {
-
-  constructor(private api: HydrusApiService) { }
+  constructor(private api: HydrusApiService) {}
 
   public setNote(hash: string, noteName: string, noteContent: string) {
-    return this.api.setNotes({hash, notes: {[noteName]: noteContent}});
+    return this.api.setNotes({ hash, notes: { [noteName]: noteContent } });
   }
 
   public addNote(hash: string, noteName: string, noteContent: string) {
-    return this.api.setNotes({hash, notes: {[noteName]: noteContent}, merge_cleverly: true})
+    return this.api.setNotes({
+      hash,
+      notes: { [noteName]: noteContent },
+      merge_cleverly: true,
+      extend_existing_note_if_possible: false,
+    });
   }
 
   public deleteNote(hash: string, noteName: string) {
-    return this.api.deleteNotes({hash, note_names: [noteName]});
+    return this.api.deleteNotes({ hash, note_names: [noteName] });
   }
-
 }
