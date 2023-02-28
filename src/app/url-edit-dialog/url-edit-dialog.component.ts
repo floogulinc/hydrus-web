b455813e59ece2899512e2d44b56a05c0d6a24e0
Paul Friederichsen <floogulinc@gmail.com>
2023-02-28 09:28:39 -0600

    Add URL editing

---
+++ src/app/url-edit-dialog/url-edit-dialog.component.ts
@@ -0,0 +1,50 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+interface UrlEditDialogData {
+  url: string;
+}
+
+@Component({
+  selector: 'app-url-edit-dialog',
+  templateUrl: './url-edit-dialog.component.html',
+  styleUrls: ['./url-edit-dialog.component.scss']
+})
+export class UrlEditDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<UrlEditDialogComponent, UrlEditDialogData>,
+    @Inject(MAT_DIALOG_DATA) public data: UrlEditDialogData
+  ) { }
+
+  static open(dialog: MatDialog, data?: UrlEditDialogData, config?: MatDialogConfig<UrlEditDialogData>) {
+    return dialog.open<UrlEditDialogComponent, UrlEditDialogData, UrlEditDialogData>(
+      UrlEditDialogComponent,
+      {
+        maxWidth: '648px',
+        width: '90vw',
+        data: {...data},
+        //autoFocus: data && data.noteName ? '.note-content' : 'first-tabbable',
+        ...config
+      }
+    );
+  }
+
+  urlForm = new FormGroup({
+    url: new FormControl(this.data.url ?? '', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+      ]
+    })
+  });
+
+  submitUrl() {
+    this.dialogRef.close(this.urlForm.getRawValue());
+  }
+
+  ngOnInit(): void {
+  }
+
+}
