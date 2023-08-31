890676290ed6e30794d1f71d9f0da4f5f5674c9c
Paul Friederichsen <floogulinc@gmail.com>
2023-08-31 12:37:36 -0500

    Update types for hydrus pages

--- src/app/hydrus-page.ts
+++ src/app/hydrus-page.ts
@@ -1,7 +1,27 @@
+export enum HydrusPageState {
+  Normal = 0,
+  Initializing = 1,
+  Searching = 2,
+  SearchCanceled = 3
+}
+
+export enum HydrusPageType {
+  ImportGallery = 1,
+  ImportSimpleDownloader = 2,
+  ImportHDD = 3,
+  Petitions = 5,
+  FileSearch = 6,
+  ImportURLs = 7,
+  Duplicates = 8,
+  ImportWatcher = 9,
+  PageOfPages = 10
+}
+
 export interface HydrusPageSimple {
   name: string;
   page_key: string;
-  page_type: 1 | 2 | 3 | 5 | 6 | 7 | 8 | 9 | 10;
+  page_type: HydrusPageType;
+  page_state: HydrusPageState
 }
 
 export interface HydrusPageListItem extends HydrusPageSimple {
