a868a6446a6c6e707bddc0cb933880d14d81f4a4
Paul Friederichsen <floogulinc@gmail.com>
2023-12-06 20:02:06 -0600

    More upload changes

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -7,13 +7,15 @@ import { HttpEventType } from '@angular/common/http';
 import { SettingsService } from '../settings.service';
 import { HydrusTagsService } from '../hydrus-tags.service';
 import { ErrorService } from '../error.service';
-import { FileValidators } from 'ngx-file-drag-drop';
+import { FileValidators, NgxFileDragDropComponent } from 'ngx-file-drag-drop';
 import { RxState } from '@rx-angular/state';
 
 interface UploadStatus {
   uploading: boolean;
   filename: string;
   percent: number;
+  upBytes: number;
+  totalBytes: number;
 }
 
 @Component({
@@ -38,6 +40,8 @@ export class UploadFileComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @ViewChild('fileInput') fileInput: NgxFileDragDropComponent;
+
   uploadForm = new FormGroup({
     fileInput: new FormControl<File[]>([], [
       FileValidators.required,
@@ -48,6 +52,8 @@ export class UploadFileComponent implements OnInit {
   readonly uploading$ = this.state.select('uploading');
   readonly percentUploaded$ = this.state.select('percent')
   readonly uploadFilename$ = this.state.select('filename')
+  readonly upBytes$ = this.state.select('upBytes')
+  readonly totalBytes$ = this.state.select('totalBytes')
 
   onSubmit() {
     console.log(this.uploadForm.value);
@@ -58,7 +64,7 @@ export class UploadFileComponent implements OnInit {
     if (this.uploadForm.value.fileInput) {
       this.state.set({uploading: true})
       while (this.uploadForm.value.fileInput.length > 0) {
-        const file = this.uploadForm.value.fileInput.shift();
+        const file = this.uploadForm.value.fileInput[0];
         try {
           this.state.set({filename: file.name})
           const response = await lastValueFrom(this.uploadService.addFile(file).pipe(
@@ -67,7 +73,7 @@ export class UploadFileComponent implements OnInit {
               if (event.type === HttpEventType.UploadProgress) {
 
                 const percent = event.total ? 100 * event.loaded / event.total : 0;
-                this.state.set({percent})
+                this.state.set({percent, upBytes: event.loaded, totalBytes: event.total})
               }
             })
           ))
@@ -87,7 +93,8 @@ export class UploadFileComponent implements OnInit {
         } catch (error) {
           this.errorService.handleHydrusError(error);
         } finally {
-          this.state.set({filename: null, percent: 0})
+          this.fileInput.removeFile(file);
+          this.state.set({filename: null, percent: 0, upBytes: 0, totalBytes: 0})
         }
       }
       this.state.set({uploading: false})
