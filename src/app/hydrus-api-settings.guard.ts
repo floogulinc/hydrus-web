78079cc6d66c446640f7145a96201b0839e1fa22
Paul Friederichsen <floogulinc@gmail.com>
2023-12-24 18:30:26 -0600

    @angular/core migration - migration-v16-guard-and-resolve-interfaces
    In Angular version 15.2, the guard and resolver interfaces (CanActivate, Resolve, etc) were deprecated. This migration removes imports and 'implements' clauses that contain them.

--- src/app/hydrus-api-settings.guard.ts
+++ src/app/hydrus-api-settings.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HydrusApiSettingsService } from './hydrus-api-settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HydrusApiSettingsGuard implements CanActivate {
+export class HydrusApiSettingsGuard  {
   constructor(private apiSettings: HydrusApiSettingsService, private router: Router) {
 
   }
